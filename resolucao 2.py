1155ed14dd46fbce1c2f7ff6a11c2fc703fb2290
Mauricio Amyr <mauricioanselmo10@gmail.com>
2019-12-17 20:51:47 -0300

    diagnostico

---
+++ resolucao 2.py
@@ -0,0 +1,8 @@
+vogais = print(input('Insira vogais aeiou:'))
+vogal = 'aiou'
+trocar_vogais = '@ & ! # *'
+while vogal == 'aeiou':
+    print(vogal(trocar_vogais))
+
+
+
---
+++ resolucao.py
@@ -0,0 +1,12 @@
+text = 'abc, abc, abc, abc, abc, abc, abc'
+line1 = 'first line:'
+line2 = 'second line:'
+line3 = 'third line:'
+line4 = 'fourth line:'
+line5 = 'fifth line:'
+line6 = 'sixth line:'
+line7 = 'seventh line:'
+result = ''
+for line in text:
+    result += line*7
+print(line1,line2,line3,line4,line6,line7)
